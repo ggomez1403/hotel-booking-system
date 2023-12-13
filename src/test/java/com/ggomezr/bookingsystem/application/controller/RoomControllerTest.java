41d26d26b6a2e796cdc5d8719f9aca5dcdf04510
Geraldine Gómez <geralgomez1403@gmail.com>
2023-12-13 10:30:24 -0500

    unit testing implemented for the project controllers

---
+++ src/test/java/com/ggomezr/bookingsystem/application/controller/RoomControllerTest.java
@@ -0,0 +1,81 @@
+package com.ggomezr.bookingsystem.application.controller;
+
+import com.ggomezr.bookingsystem.application.service.RoomService;
+import com.ggomezr.bookingsystem.domain.dto.RoomDto;
+import com.ggomezr.bookingsystem.domain.entity.Room;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class RoomControllerTest {
+    @Mock
+    private RoomService roomService;
+
+    @InjectMocks
+    private RoomController roomController;
+
+    @Test
+    public void testGetAllRooms() {
+        List<Room> expectedRooms = List.of(new Room(), new Room());
+        when(roomService.getAllRooms()).thenReturn(expectedRooms);
+
+        List<Room> actualRooms = roomController.getAllRooms();
+
+        assertEquals(expectedRooms, actualRooms);
+        verify(roomService).getAllRooms();
+    }
+    @Test
+    public void testGetRoomById() throws Exception {
+        Room expectedRoom = Room.builder().id(1).build();
+        when(roomService.getRoomById(1)).thenReturn(Optional.of(expectedRoom));
+
+        Optional<Room> actualRoom = roomController.getRoomById(1);
+
+        assertTrue(actualRoom.isPresent());
+        assertEquals(expectedRoom, actualRoom.get());
+    }
+
+    @Test
+    public void testCreateRoom() {
+        RoomDto newRoomDto = new RoomDto(1, "Room 1", true,
+                "Single", 2, new BigDecimal(50));
+
+        roomController.createRoom(newRoomDto);
+
+        verify(roomService).createRoom(eq(newRoomDto));
+    }
+
+    @Test
+    public void testUpdateRoom() throws Exception {
+        Integer roomId = 1;
+        RoomDto updatedRoomDto = new RoomDto(1, "Updated Name",
+                false, "Double", 4, new BigDecimal(75));
+
+        Room existingRoom = Room.builder().id(roomId).build();
+
+        when(roomService.getRoomById(roomId))
+                .thenReturn(Optional.of(existingRoom));
+
+        roomController.updateRoom(roomId, updatedRoomDto);
+
+        verify(roomService).updateRoom(roomId, updatedRoomDto);
+    }
+    @Test
+    public void testDeleteRoom() {
+        roomController.deleteRoom(1);
+
+        verify(roomService).deleteRoom(eq(1));
+    }
+}
