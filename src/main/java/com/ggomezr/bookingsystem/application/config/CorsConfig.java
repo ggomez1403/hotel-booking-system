e72d7c01b2bed264ab348b7ca2f9ae5850471019
Geraldine Gómez <geralgomez1403@gmail.com>
2023-12-15 17:16:19 -0500

    Successful Swagger URL fix for HTTPS and CORS configuration adjustment

--- src/main/java/com/ggomezr/bookingsystem/application/config/CorsConfig.java
+++ src/main/java/com/ggomezr/bookingsystem/application/config/CorsConfig.java
@@ -6,6 +6,12 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+/*
+    Configuring Cross-Origin Resource Sharing (CORS)
+    - Defines rules that allow or restrict resource requests from a domain other than the server's
+    - Controls how browsers allow a web page to access resources on a different domain.
+*/
+
 @Configuration
 public class CorsConfig {
 
